dc618a5067e8a0376048b7681be3d439aae8e918
mahmuda_munny <67213233+munny006@users.noreply.github.com>
2023-02-26 11:58:24 +0600

    meal information fetch done

--- js/meals.js
+++ js/meals.js
@@ -17,7 +17,7 @@ const displayMeals = meals =>{
     <div class="card-body">
       <h5 class="card-title">${meal.strMeal}</h5>
       <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-      <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#mealsDetails">
+      <button onclick = "loadMealDetail(${meal.idMeal})" type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#mealsDetails">
       Read More
     </button>
     </div>
@@ -33,5 +33,13 @@ const searchText = document.getElementById('search-field').value;
 //search meals
 console.log(searchText);
 loadMeals(searchText)
+}
+const loadMealDetail = idMeal => {
+  const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`;
+  
+  fetch(url)
+  .then(res => res.json())
+  .then(data=>console.log(data));
+  console.log(idMeal);
 }
 loadMeals('rice');
